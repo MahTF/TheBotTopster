ac1f9d2a36a98a051ae96b2f4dded5e520daac1a
Marcelo T Ferreira <mah.tf@hotmail.com>
2022-05-18 01:13:45 -0300

    Created the top command

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { client } from "./config";
+import { TopReplies } from './utils/TopReplies';
+
+const TopRepliesLength = TopReplies.length;
 
 client.connect();
 
@@ -6,7 +9,27 @@ client.on('message', (channel, tags, message, self) => {
   // ignorar as proprias mensagens
   if (self) return;
 
-  if (message.toLowerCase().includes("oi")) {
-    client.say(channel, `Olá @${tags.username}`);
+  // Caso a mensagem tenha "top" nela, o bot responderá
+  if (message.toLowerCase().includes("top")) {
+    let index = Math.random() * TopRepliesLength;
+    index = Math.round(index);
+
+    if (index >= TopRepliesLength) {
+      index = index - 1;
+    }
+
+    let reply = TopReplies[index];
+    if (reply.includes('@nome')) {
+      reply = reply.replace('@nome', `@${tags.username}`);
+    }
+
+    try {
+      client.say(
+        channel,
+        reply
+      );
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
---
+++ src/utils/TopReplies.ts
@@ -0,0 +1,11 @@
+const TopReplies = [
+  'Top!',
+  '@nome, topzera?',
+  '@nome, eu to voando alto, mlk!',
+  '@nome, fiquei sabendo de uma parada bem TOOOOOOOOOOOOOOOOP!',
+  'mas você @nome, é top das galaxias!!',
+  // mahtfTop é um emote de inscrito do canal de MahTF
+  'Você já disse a @ManukiTanuki que é topster hoje? mahtfTop'
+]
+
+export { TopReplies };
