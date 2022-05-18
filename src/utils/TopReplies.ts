e72b4124946d367bfd8bd9d9d29373cb41c43679
Marcelo T Ferreira <mah.tf@hotmail.com>
2022-05-18 16:41:57 -0300

    Added more top replies

--- src/utils/TopReplies.ts
+++ src/utils/TopReplies.ts
@@ -4,6 +4,16 @@ const TopReplies = [
   '@nome, eu to voando alto, mlk!',
   '@nome, fiquei sabendo de uma parada bem TOOOOOOOOOOOOOOOOP!',
   'mas você @nome, é top das galaxias!!',
+  '@nome é cria mermo. Top Demais!',
+  'Aí o @nome topzou demais. Pega o Oakley que a topzisse brilhou, brother!',
+  '@nome tá top assim porque comserteza saiu comigo.',
+  '@nome é muito top. Seu carro é automático?',
+  '@nome, sua topzisse bateu no meu Copo Stanley e encheu ele de cerveja.',
+  '@nome Tudo bem ser top assim. Tenho até amigos que são...',
+  'Pra falar essas coisas @nome, eu já sei que é amigo do @ManukiTaTop.',
+  'Não tem nada mais Lek Topzera do que viajar pra São Paulo de férias todo mês',
+  'Todo Lek Topster que eu conheço tem um Fox, sabia?',
+  'Tá top, tá top, tá é muita onda, essa live tá bombando e eu sou muito top!',
   // mahtfTop é um emote de inscrito do canal de MahTF
   'Você já disse a @ManukiTanuki que é topster hoje? mahtfTop'
 ]
