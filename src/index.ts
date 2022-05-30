75596e79a0b972a489b23e960d0995a5d527e823
Marcelo T Ferreira <mah.tf@hotmail.com>
2022-05-30 18:05:24 -0300

    Added spawn command

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ client.on('message', (channel, tags, message, self) => {
     || message.toLowerCase().startsWith(`@${process.env.NODE_TWITCH_USERNAME as string}`)
   ) return;
 
+  //#region Comando: TOP
   // Caso a mensagem tenha "top" nela, o bot responderá
   if (
     message.toLowerCase().includes("top") ||
@@ -38,4 +39,16 @@ client.on('message', (channel, tags, message, self) => {
       console.log(err);
     }
   }
+  //#endregion
+
+  //#region Comando !spawn
+  /// Este comando serve para resgatar um som na live de Gata_amaldicoada quando um user digita uma mensagem Específica
+  if (
+    channel === "#gata_amaldicoada" &&
+    tags.username === "PokemonCommunityGame" &&
+    message.toLowerCase().includes("tente capturar usando !pokecatch")
+  ) {
+    client.say(channel, "!spawn");
+  }
+  //#endregion
 });
