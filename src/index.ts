43f208c722d1675a6c20ebe34b0eabfd37d8ae11
Marcelo T Ferreira <mah.tf@hotmail.com>
2022-06-24 22:03:49 -0300

    Fixed join phrase

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ import { SpawnCommand } from './commands/SpawnCommand';
 
 client.connect();
 
-client.once('join', (channel) =>
-  client.say(channel, "Olá estou chegando aqui para deixar tudo topster! Provavelmente teve atualização do bot! TwitchGlitch"));
+client.on('join', (channel) => {
+  client.say(
+    channel,
+    "Olá estou chegando aqui para deixar tudo topster! Provavelmente teve atualização do bot! SirPrise"
+  )
+});
 
 // Chamando os comandos para poderem ser executados
 // TopCommand;
