2b104a5b3c13ce6705b1ba7c84f893688d36f13b
Marcelo Ferreira <mah.tf@hotmail.com>
2022-10-03 00:56:15 -0300

    Edited Weekly missions

--- src/utils/PokemonMissions.ts
+++ src/utils/PokemonMissions.ts
@@ -2,26 +2,40 @@ import { client } from '../config';
 import { IPokemon } from './IPokemon';
 
 /* Missão Semanal
- * Data final: Domingo, 02 de Outubro de 2022 19:00 GMT-3
- * Missão 1: Troque 5 pokemon de metal para ganhar um Noivern Tipo Voador / Dragão
- * Missão 2: Pegue pokemon pesado, na pokedex, não tem peso ainda.
+ * Data final: Domingo, 09 de Outubro de 2022 19:00 GMT-3
+ * Missão 1: Pegue 05 pokémons do tipo de Fogo para obter um Cyndaquil, pokémon de fogo
+ * Missão 2: Tente capturar 30 pokémons para um Sawsbuck, pokémon Normal / Grama
+ * Missão 3: Troque 09 pokémons tipo Fada para um Sylveon, pokémon Fada
  * Missões adicionais não estão com tipo/estado do pokémon.
  */
 
 // Mês: Inicia no 0, Janeiro: 0, Fevereiro: 1, ...
-const deadLine = new Date(2022, 9, 2, 19);
+const deadLine = new Date(2022, 9, 9, 19);
 
 function PokemonMissions(channel: string, pokemon: IPokemon) {
   const DateNow = new Date();
   const ValidTime = deadLine >= DateNow;
 
   if (ValidTime) {
-    if (pokemon.Tipo.toLowerCase().includes("metal")) {
+    if (pokemon.Tipo.toLowerCase().includes("fogo")) {
       client.action(
         channel,
-        `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Metal, capture (05) e troque-o para ganhar 01 Noivern. Pokémon tipo Voador / Dragão`
+        `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Fogo, capture (05) para ganhar 01 Cyndaquil. Pokémon tipo Fogo`
       );
-    } else {
+      if (pokemon.Tipo.toLowerCase().includes("fada")) {
+        client.action(
+          channel,
+          `${pokemon.Name} é um Pokémon que pode estar em duas missões!`
+        )
+      }
+    } if (pokemon.Tipo.toLowerCase().includes("fada") &&
+      !pokemon.Tipo.toLowerCase().includes("fogo")) {
+      client.action(
+        channel,
+        `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Fada, capture e troque-o (09) para ganhar 01 Sylveon. Pokémon tipo Fada`
+      );
+    }
+    else {
       client.action(
         channel,
         'Atenção, tem missão de tentar capturar 30 pokémons e capturar pokemon pesado!'
