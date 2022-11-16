771b55bb6c84b972d64fa111987df42ec1e49b96
Marcelo Ferreira <mah.tf@hotmail.com>
2022-11-16 17:44:18 -0300

    Edited Weekly missions

--- src/utils/PokemonMissions.ts
+++ src/utils/PokemonMissions.ts
@@ -2,35 +2,35 @@ import { client } from '../config';
 import { IPokemon } from './IPokemon';
 
 /* Missão Semanal
- * Data final: Domingo, 06 de Novembro de 2022 19:00 GMT-3
- * Missão 1: Capturar 06 pokémons de metal para um Total Reset
- * Missão 2: Trocar 8 pokémons do tipo dragão para um Totodile, tipo de água
+ * Data final: Domingo, 20 de Novembro de 2022 19:00 GMT-3
+ * Missão 1: Trocar 5 pokémons do tipo fogo para um Torchic, tipo de fogo
+ * Missão 2: Capturar 5 pokémons lutadores para 02 IV Reset
  * Missões adicionais não estão com tipo/estado do pokémon.
  */
 
 // Mês: Inicia no 0, Janeiro: 0, Fevereiro: 1, ...
-const deadLine = new Date(2022, 10, 6, 19);
+const deadLine = new Date(2022, 10, 20, 19);
 
 function PokemonMissions(channel: string, pokemon: IPokemon) {
   const DateNow = new Date();
   const ValidTime = deadLine >= DateNow;
 
   if (ValidTime) {
-    if (pokemon.Tipo.toLowerCase().includes("metal")) {
-      if (pokemon.Tipo.toLowerCase().includes("dragão")) {
+    if (pokemon.Tipo.toLowerCase().includes("fogo")) {
+      if (pokemon.Tipo.toLowerCase().includes("lutador")) {
         client.action(
           channel,
-          `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Metal e o tipo Dragão, capture e troque (08) para ganhar 01 Pokemon e um item.`
+          `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Fogo e o tipo Lutador, capture e troque (05) para ganhar 01 Pokemon e um item.`
         );
       }
       client.action(
         channel,
-        `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Metal, capture (06) para ganhar 01 Total Reset`
+        `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Fogo, capture e troque-o (05) para ganhar 01 Torchic, Pokémon tipo Fogo`
       );
     } else if (pokemon.Tipo.toLowerCase().includes("dragão"))
       client.action(
         channel,
-        `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Dragão, capture e troque-o (08) para ganhar 01 Totodile. Pokémon tipo de água.`
+        `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Lutador, capture-o (05) para ganhar 02 IV Reset.`
       );
   } else {
     client.action(
