645ce80a4546aafcdb418ecd21deb5ed17272ad9
Marcelo Ferreira <mah.tf@hotmail.com>
2022-11-27 19:33:57 -0300

    Edited Weekly missions

--- src/utils/PokemonMissions.ts
+++ src/utils/PokemonMissions.ts
@@ -2,41 +2,37 @@ import { client } from '../config';
 import { IPokemon } from './IPokemon';
 
 /* Missão Semanal
- * Data final: Domingo, 27 de Novembro de 2022 19:00 GMT-3
- * Missão 1: Capturar 5 pokémons sombrios para 01 Total Reset
- * Missão 2: Trocar 7 pokémons água para 01 Sobble. Pokémon tipo água
- * Missão 3: Tentar 30 capturas para 03 Repeat Ball
+ * Data final: Domingo, 04 de Dezembro de 2022 19:00 GMT-3
+ * Missão 1: Troque 03 pokémons com status base aciama de 500 para 01 Litten. Pokémon de Fogo
+ * Missão 2: Pegue 04 pokémons eletricos para ganhar 03 Repeat Ball
  * Missões adicionais não estão com tipo/estado do pokémon.
  */
 
 // Mês: Inicia no 0, Janeiro: 0, Fevereiro: 1, ...
-const deadLine = new Date(2022, 10, 27, 19);
+const deadLine = new Date(2022, 12, 4, 19);
 
 function PokemonMissions(channel: string, pokemon: IPokemon) {
   const DateNow = new Date();
   const ValidTime = deadLine >= DateNow;
+  const statusBaseInNumber = Number(pokemon.StatusBase);
 
   if (ValidTime) {
-    if (pokemon.Tipo.toLowerCase().includes("sombrio")) {
-      if (pokemon.Tipo.toLowerCase().includes("água")) {
+    if (pokemon.Tipo.toLowerCase().includes("elétrico")) {
+      if (statusBaseInNumber >= 500) {
         client.action(
           channel,
-          `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Sombrio e o tipo Água, capture e troque (07) para ganhar 01 Pokemon e um item.`
+          `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Elétrico e tem ${statusBaseInNumber} de status base, capture e troque (04) para ganhar 01 Pokemon e um item.`
         );
       }
       client.action(
         channel,
-        `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Sombrio, capture-o (05) para ganhar 01 Total Reset.`
+        `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Elétrico, capture e troque-o (04) para ganhar 03 Repeat Balls.`
       );
-    } else if (pokemon.Tipo.toLowerCase().includes("água"))
+    } else if (statusBaseInNumber >= 500) {
       client.action(
         channel,
-        `${pokemon.Name} é um Pokémon que possui (ou pode possuir) o tipo Água, capture e troque-o (07) para ganhar 01 Sobble. Pokémon tipo Água.`
-      ); else {
-      client.action(
-        channel,
-        `Existe uma missão de tentar 30 capturas para ganhar 03 Repeat Balls!`
-      )
+        `${pokemon.Name} é um Pokémon que possui ${statusBaseInNumber} de status base, capture-o (03) para ganhar 01 Litten. Pokémon de Fogo.`
+      );
     }
   } else {
     client.action(
